86551204269f4746f8f669e4625f32afb121bb1b
Joker13666 <49472625+Joker13666@users.noreply.github.com>
2019-05-22 10:53:06 +0300

    Add files via upload

---
+++ serverpart.py
@@ -0,0 +1,23 @@
+import socket
+
+HOST = input('Введите HOST ')
+PORT = 8080
+
+sock = socket.socket()
+sock.bind((HOST,PORT))
+
+sock.listen(1)
+print('Ожидание подключения..')
+
+conn, adr = sock.accept()
+print('Подключен'+str(adr))
+
+text = 'Отправьте матрицу'
+conn.send(text.encode())
+
+text = conn.recv(65565).decode()
+lst = list(map(lambda x: x.strip().split(' '),text[1:-1].split(';')))
+det = int(lst[0][0])*int(lst[1][1])-int(lst[0][1])*int(lst[1][0])
+text = '[ '+str(int(lst[0][0])*det) + ' ' + str(int(lst[0][1])*det) + '; ' + str(int(lst[1][0])*det) + ' ' + str(int(lst[1][1])*det) + ' ]'
+
+conn.send(text.encode())
